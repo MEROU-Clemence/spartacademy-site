32cc5f8bb1b1605b468449bba359d3b8e5445ff5
MEROU-Clemence <clemence.merou@gmail.com>
2023-11-10 11:55:21 +0100

    premiere fixture faite et d:f:l effectué

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,96 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Address;
+use Faker\Factory;
+use App\Entity\User;
+use Faker\Generator;
+use App\Entity\InfoUser;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    // proriété pour encoder le mot de passe
+    private $encoder;
+    // propriété pour le faker
+    private Generator $faker;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // ici on va appeler toutes nos fonctions load
+        // fonction loadAddress
+        $this->loadAddress($manager);
 
         $manager->flush();
     }
+
+    // méthode qui alimente Address
+    public function loadAddress(ObjectManager $manager)
+    {
+        for ($i = 1; $i <= 4; $i++) {
+            $address = new Address();
+            $address->setAddress0($this->faker->streetAddress);
+            $address->setAddress1($this->faker->streetAddress);
+            $address->setAddress2($this->faker->streetAddress);
+            $address->setZipCode($this->faker->postcode);
+            $address->setCity($this->faker->city);
+            $address->setCountry($this->faker->country);
+            $manager->persist($address);
+            $this->addReference('address_' . $i, $address);
+        }
+    }
+
+
+
+
+
+
+
+
+
+    // // méthode qui alimente InfoUser
+    // public function loadInfoUser(ObjectManager $manager)
+    // {
+    //     for ($i = 1; $i <= 4; $i++) {
+    //         $infoUser = new InfoUser();
+    //         $infoUser->setName($this->faker->lastName);
+    //         $infoUser->setFirstname($this->faker->firstName);
+    //         $infoUser->setPhone($this->faker->phoneNumber);
+    //         $infoUser->setBirthDate($this->faker->dateTimeBetween($startDate = '-100 years', $endDate = '-6 years', $timezone = null));
+    //         $manager->persist($infoUser);
+    //         $this->addReference('infoUser_' . $i, $infoUser);
+    //     }
+    // }
+
+    // // méthode qui alimente User
+    // public function loadUser(ObjectManager $manager)
+    // {
+    //     // on créé un admin
+    //     $user = new User();
+    //     $user->setEmail('admin@admin.com')
+    //         ->setPassword($this->encoder->hashPassword($user, 'admin'))
+    //         ->setRoles(['ROLE_ADMIN'])
+    //         ->setInfoUser($this->getReference('infoUser_1'));
+    //     $manager->persist($user);
+    //     $this->addReference('user_1', $user);
+
+    //     // on créé les 3 clients (références de 2 à 4)
+    //     for ($i = 2; $i <= 4; $i++) {
+    //         $user = new User();
+    //         $user->setEmail($this->faker->email);
+    //         $user->setPassword($this->encoder->hashPassword($user, 'prof'));
+    //         $user->setRoles(['ROLE_CLIENT']);
+    //         $user->setInfoUser($this->getReference('infoUser_' . $i));
+    //         $manager->persist($user);
+    //         $this->addReference('user_' . $i, $user);
+    //     }
+    // }
 }
