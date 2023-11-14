ab73b7c0354c4e916139501cb43a45320b5853cb
MEROU-Clemence <clemence.merou@gmail.com>
2023-11-14 16:57:12 +0100

    debut de la galerie sur le menu

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,21 @@ import './styles/app.sass';
 import { Tooltip, Toast, Popover } from 'bootstrap';
 // start the Stimulus application
 import './bootstrap';
+
+// galerie sur l'index qui fait défiler les images
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryContainer = document.querySelector('.gallery-container');
+
+    function rotateGallery() {
+        const firstImage = galleryContainer.firstElementChild;
+        galleryContainer.style.transition = 'transform 0.5s ease-in-out';
+        galleryContainer.style.transform = 'translateX(-' + firstImage.clientWidth + 'px)';
+        setTimeout(() => {
+            galleryContainer.appendChild(firstImage);
+            galleryContainer.style.transition = 'none';
+            galleryContainer.style.transform = 'translateX(0)';
+        }, 500);
+    }
+
+    setInterval(rotateGallery, 3000);
+});
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Gallery;
 use App\Form\ContactType;
+use App\Repository\GalleryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,25 +14,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET', 'POST'])]
-    public function index(Request $request): Response
+    public function index(Request $request, GalleryRepository $galleryRepository): Response
     {
         // Créez un tableau pour stocker les données du formulaire
         $formData = [];
 
         // je déclare mes variables
         $form = $this->createForm(ContactType::class, $formData);
+        $gallerys = $galleryRepository->findAll();
 
-        // Gérez la soumission du formulaire
+        // soumission du formulaire
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
 
+            // TODO: envoyer en BDD avec onglet pour recevoir coté admin.
             // TODO: faire popup pour dire que le message est bien envoyé
             return $this->redirectToRoute('nom_de_popup_qui_dit_ok_message_bien_envoyé');
         }
 
         return $this->render('home/index.html.twig', [
             'form' => $form->createView(),
+            'gallerys' => $gallerys
         ]);
     }
 }
