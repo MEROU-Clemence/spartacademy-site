4262ea9ba2c43387b250ba94e4f1db3fc9210f15
MEROU-Clemence <clemence.merou@gmail.com>
2023-11-16 11:21:50 +0100

    carousel fonctionnel

--- assets/app.js
+++ assets/app.js
@@ -12,23 +12,3 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 // start the Stimulus application
 import './bootstrap';
 
-// galerie sur l'index qui fait défiler les images
-document.addEventListener("DOMContentLoaded", function () {
-    const galleryImages = document.querySelectorAll('.gallery-container img');
-    let currentImageIndex = 0;
-
-    function showNextImage() {
-        galleryImages[currentImageIndex].classList.remove('visible');
-        galleryImages[currentImageIndex].classList.add('invisible');
-
-        currentImageIndex = (currentImageIndex + 1) % galleryImages.length;
-
-        galleryImages[currentImageIndex].classList.remove('invisible');
-        galleryImages[currentImageIndex].classList.add('visible');
-    }
-
-    // cela change l'image toutes les 10 sec
-    setInterval(showNextImage, 10000);
-
-});
-console.log('chat');
---
+++ public/assets/carousel.js
@@ -0,0 +1,19 @@
+// galerie sur l'index qui fait défiler les images
+document.addEventListener("DOMContentLoaded", function () {
+    const galleryImages = document.querySelectorAll('.gallery-container img');
+    let currentImageIndex = 0;
+
+    function showNextImage() {
+        galleryImages[currentImageIndex].classList.remove('visible');
+        galleryImages[currentImageIndex].classList.add('invisible');
+
+        currentImageIndex = (currentImageIndex + 1) % galleryImages.length;
+
+        galleryImages[currentImageIndex].classList.remove('invisible');
+        galleryImages[currentImageIndex].classList.add('visible');
+    }
+
+    // cela change l'image toutes les 10 sec
+    setInterval(showNextImage, 10000);
+
+});
